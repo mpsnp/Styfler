cf9ba72131264a646fc8c377d1b7c58d15fc7a60
George Kiriy <gkiriy@smedialink.com>
2019-03-18 11:40:16 +0300

    Added shadow styles struct

--- Sources/Theme.swift
+++ Sources/Theme.swift
@@ -16,18 +16,28 @@ public protocol AttributedTextStyle: TextStyle {
     var letterSpacing: CGFloat { get }
 }
 
+public protocol ShadowStyle {
+    var color: UIColor { get }
+    var opacity: CGFloat { get }
+    var radius: CGFloat { get }
+    var offset: CGSize { get }
+}
+
 public protocol Theme {
     associatedtype Colors
     associatedtype CornerRadiuses
     associatedtype TextStyles
+    associatedtype ShadowStyles
 
     var colors: Colors { get }
     var cornerRadiuses: CornerRadiuses { get }
     var textStyles: TextStyles { get }
+    var shadowStyles: ShadowStyles { get }
 }
 
 public extension Style {
     public typealias Colors = Theme.Colors
     public typealias CornerRadiuses = Theme.CornerRadiuses
     public typealias TextStyles = Theme.TextStyles
+    public typealias ShadowStyles = Theme.ShadowStyles
 }
