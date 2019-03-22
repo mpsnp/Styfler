7b4adf87f2ad9fddd129e97a8c1e362fb87f27ee
Valeriy Mikholapov <vmikholapov@smedialink.com>
2019-03-22 19:41:50 +0300

    Added styling function with updated naming; marked previous ones as deprecated.

--- Sources/Styles/Style+UIView.swift
+++ Sources/Styles/Style+UIView.swift
@@ -11,44 +11,109 @@ import UIKit
 
 extension UIView: Stylable {}
 
+// MARK: - Deprecated
+
 public extension Style where Stylable: UIView {
+
+    @available(*, deprecated, renamed: "clipsToBounds(_:)")
     static func view(clipsToBounds: Bool = true) -> Style {
         return Style(set: \.clipsToBounds, to: clipsToBounds)
     }
 
+    @available(*, deprecated, renamed: "isOpaque(_:)")
     static func view(isOpaque: Bool = true) -> Style {
         return Style(set: \.isOpaque, to: isOpaque)
     }
 
+    @available(*, deprecated, renamed: "alpha(_:)")
     static func view(alpha: CGFloat) -> Style {
         return Style(set: \.alpha, to: alpha)
     }
 
+    @available(*, deprecated, renamed: "backgroundColor(_:)")
     static func view(backgroundColor: KeyPath<Theme.Colors, UIColor>) -> Style {
         return Style(set: \.backgroundColor, from: \.colors >>> backgroundColor)
     }
 
+    @available(*, deprecated, renamed: "tintColor(_:)")
     static func view(tintColor: KeyPath<Theme.Colors, UIColor>) -> Style {
         return Style(set: \.tintColor, from: \.colors >>> tintColor)
     }
+
+}
+
+// MARK: -
+
+public extension Style where Stylable: UIView {
+
+    static func clipsToBounds(_ clipsToBounds: Bool = true) -> Style {
+        return Style(set: \.clipsToBounds, to: clipsToBounds)
+    }
+
+    static func isOpaque(_ isOpaque: Bool = true) -> Style {
+        return Style(set: \.isOpaque, to: isOpaque)
+    }
+
+    static func backgroundColor(_ backgroundColor: KeyPath<Theme.Colors, UIColor>) -> Style {
+        return Style(set: \.backgroundColor, from: \.colors >>> backgroundColor)
+    }
+
+    static func alpha(_ alpha: CGFloat) -> Style {
+        return Style(set: \.alpha, to: alpha)
+    }
+
+    static func tintColor(_ tintColor: KeyPath<Theme.Colors, UIColor>) -> Style {
+        return Style(set: \.tintColor, from: \.colors >>> tintColor)
+    }
+
 }
 
 // MARK: - Layer wrappers
+
+// MARK: - Deprecated
+
 public extension Style where Stylable: UIView {
+    @available(*, deprecated, renamed: "cornerRadius(_:)")
     static func layer(cornerRadius: KeyPath<CornerRadiuses, CGFloat>) -> Style {
         return \.layer <<< .layer(cornerRadius: cornerRadius)
     }
 
+    @available(*, deprecated, renamed: "masksToBounds(_:)")
     static func layer(masksToBounds: Bool) -> Style {
         return \.layer <<< .layer(masksToBounds: masksToBounds)
     }
 
+    @available(*, deprecated, renamed: "border(withWidth:andColor:)")
     static func layer(borderWidth: CGFloat = 1, borderColor: KeyPath<Colors, UIColor>) -> Style {
         return \.layer <<< .layer(borderWidth: borderWidth, borderColor: borderColor)
     }
 
+    @available(*, deprecated, renamed: "border(withWidth:andColor:)")
     static func layer(borderWidth: KeyPath<Theme, CGFloat>, borderColor: KeyPath<Colors, UIColor>) -> Style {
         return \.layer <<< .layer(borderWidth: borderWidth, borderColor: borderColor)
+    }
+
+}
+
+// MARK: -
+
+public extension Style where Stylable: UIView {
+    static func cornerRadius(_ cornerRadius: KeyPath<CornerRadiuses, CGFloat>) -> Style {
+        return \.layer <<< .init(set: \.cornerRadius, from: \.cornerRadiuses >>> cornerRadius)
+    }
+
+    static func masksToBounds(_ masksToBounds: Bool) -> Style {
+        return \.layer <<< .init(set: \.masksToBounds, to: masksToBounds)
+    }
+
+    static func border(withWidth width: CGFloat, andColor color: KeyPath<Colors, UIColor>) -> Style {
+        return \.layer <<< .init(set: \.borderWidth, to: width)
+            <> \.layer <<< .init(set: \.borderColor, from: \.colors >>> color >>> \.cgColor)
+    }
+
+    static func border(withWidth width: KeyPath<Theme, CGFloat>, andColor color: KeyPath<Colors, UIColor>) -> Style {
+        return \.layer <<< .init(set: \.borderWidth, from: width)
+            <> \.layer <<< .init(set: \.borderColor, from: \.colors >>> color >>> \.cgColor)
     }
 
     static func shadow<S: ShadowStyle>(style: KeyPath<ShadowStyles, S>) -> Style {
